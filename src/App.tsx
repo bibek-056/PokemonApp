dbf36732abd53fadbd7313e68a02a12779b288f6
bibek-056 <adh.bibek056@gmail.com>
2024-01-08 21:50:19 +0545

    feat(pokemon cards): fetches and displays pokemon cards in the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-
+import { useQuery } from "react-query";
+import PokemonCard from "./Components/PokemonCard";
 
 function App() {
-
+  const { isLoading: pokemonLoading, data: pokemonData } = useQuery(
+    "pokemonData",
+    async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return response.json();
+    }
+  );
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <img src="/pokemon.jpeg" alt="pokemon"/>
+    <div className="w-screen h-screen flex flex-col justify-start ">
+      {pokemonLoading ? (
+        <div className="flex w-full h-full justify-center items-center">
+          <div className="flex justify-start px-4 py-2">
+            <img src="/pokemon.png" alt="pokemon" />
+          </div>
+          <p className="text-yellow-300 font-bold text-xl">
+            Pokemons Loading...
+          </p>
+        </div>
+      ) : (
+        <div className="w-full h-full flex flex-col justify-start items-start">
+          <div className="flex justify-start items-center px-6 py-2">
+            <img className="h-20" src="/pokemon.png" alt="pokemon" />
+          </div>
+          <div className="w-full flex flex-wrap justify-center items-center gap-6">
+            {pokemonData?.results?.map((onePokemon: any, index: number) => (
+              <div key={index} className="w-1/4">
+                <PokemonCard pokemonUrl={onePokemon.url} />{" "}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/PokemonCard.tsx
@@ -0,0 +1,40 @@
+import { useQuery } from "react-query";
+
+const PokemonCard = ({ pokemonUrl }) => {
+  console.log(pokemonUrl);
+  const { data: onePokemonData, isLoading } = useQuery(
+    ["onePokemon", pokemonUrl],
+    async () => {
+      const response = await fetch(`${pokemonUrl}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return response.json();
+    }
+  );
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="w-full border bg-white h-full rounded-md flex flex-col gap-2 items-center justify-center text-black p-2">
+      <p className="font-semibold text-xl px-4 border-b pb-2 w-full text-left">
+        {onePokemonData.forms[0].name.toUpperCase()}
+      </p>
+      <img
+        className="h-40 border rounded-3xl shadow-2xl"
+        src={onePokemonData.sprites.other["official-artwork"].front_default}
+        alt="pokemon"
+      />
+      <p className="font-medium text-xl border-b pb-2">Types:</p>
+      <div className="w-full flex justify-between">
+        {onePokemonData.types.map((oneType) => (
+          <p className="w-full text-center font-semibold leading-8 text-2xl ">{oneType.type.name.toUpperCase()}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
