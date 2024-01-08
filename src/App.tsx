3eec27e1160d8d6636b97434bc8b20663aaa0e0d
bibek-056 <adh.bibek056@gmail.com>
2024-01-08 19:57:13 +0545

    feat(web app): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <img src="/pokemon.jpeg" alt="pokemon"/>
+    </div>
+  )
+}
+
+export default App
